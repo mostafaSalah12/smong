0f61413c3f4d2afef1fd90708eb98b0f6bcce0e5
msalah <msalah@Java-1.v-apps.local>
2017-10-25 15:10:10 +0200

    user Repository commit

--- kitchan/src/main/java/com/pltfh/kitchen/repositroy/UserRepository.java
+++ kitchan/src/main/java/com/pltfh/kitchen/repositroy/UserRepository.java
@@ -1,5 +1,11 @@
 package com.pltfh.kitchen.repositroy;
 
-public interface UserRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pltfh.kitchen.model.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
 
 }
